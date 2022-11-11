f1f14c9c7ab8eeae6c2bb7a89a675aa6e522eac9
Joseph Nemec <jnemec84@gmail.com>
2022-11-11 11:59:33 -0600

    add pages folders

---
+++ cyborg-gaming/pages/details/index.js
@@ -0,0 +1,5 @@
+const DetailsPage = () => {
+  return <div>Details Page</div>;
+};
+
+export default DetailsPage;
---
+++ cyborg-gaming/pages/streams/index.js
@@ -0,0 +1,5 @@
+const StreamsPage = () => {
+  return <div>Streams Page</div>;
+};
+
+export default StreamsPage;
