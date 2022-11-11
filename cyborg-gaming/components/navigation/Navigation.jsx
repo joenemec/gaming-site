937f2903cba918348161565f65f0dc76d5ecdfee
Joseph Nemec <jnemec84@gmail.com>
2022-11-11 16:25:27 -0600

    add navigation component

--- cyborg-gaming/components/navigation/Navigation.jsx
+++ cyborg-gaming/components/navigation/Navigation.jsx
@@ -1,42 +1,72 @@
+import React, { useState } from "react";
 import Image from "next/image";
 
 import Link from "next/link";
 
 import { NavigationLinks } from "./NavigationLinks";
-import { TbSearch } from "react-icons/tb";
+import { TbSearch, TbAlignLeft } from "react-icons/tb";
+import { CgMenuLeft, CgClose } from "react-icons/cg";
 
 import styles from "./Navigation.module.scss";
 
 const Navigation = () => {
+  const [isResponsive, setIsResponsive] = useState(false);
+
+  const menuHandler = () => {
+    setIsResponsive(true);
+  };
+
+  const closeHandler = () => {
+    setIsResponsive(false);
+  };
   return (
     <header className={styles.header}>
-      <div className={styles.left}>
-        <div className={styles.logo}>
-          <Image src="/images/logo.png" width={191} height={50.73} />
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <div className={styles.logo}>
+            <Image
+              src="/images/logo.png"
+              width={191}
+              height={50.73}
+              alt="Company Logo"
+            />
+          </div>
+          <div className={styles.search}>
+            <input placeholder="Type Something"></input>
+          </div>
         </div>
-        <div className={styles.search}>
-          <input placeholder="Type Something"></input>
-        </div>
+
+        <nav
+          className={
+            isResponsive ? [styles.nav, styles.active].join(" ") : [styles.nav]
+          }
+        >
+          <ul>
+            {NavigationLinks.map((link, index) => {
+              return (
+                <li key={index}>
+                  <Link href={link.url}>{link.title}</Link>
+                  {link.image ? (
+                    <Image
+                      className={styles.profile}
+                      src={link.image}
+                      width={30}
+                      height={30}
+                    />
+                  ) : null}
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
       </div>
-      <nav className={styles.nav}>
-        <ul>
-          {NavigationLinks.map((link, index) => {
-            return (
-              <li key={index}>
-                <Link href={link.url}>{link.title}</Link>
-                {link.image ? (
-                  <Image
-                    className={styles.profile}
-                    src={link.image}
-                    width={30}
-                    height={30}
-                  />
-                ) : null}
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      <button className={styles.menu}>
+        {!isResponsive ? (
+          <CgMenuLeft size={35} onClick={menuHandler} />
+        ) : (
+          <CgClose size={35} onClick={closeHandler} />
+        )}
+      </button>
     </header>
   );
 };
